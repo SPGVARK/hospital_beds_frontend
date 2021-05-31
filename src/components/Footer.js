340621fe6dc4a8762b9a1e21810b21a9990fcf11
SPGVARK <83496037+SPGVARK@users.noreply.github.com>
2021-05-31 19:04:06 +0530

    Add files via upload

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,8 +7,9 @@ export default function Footer(){
     return(
     <>
          <footer id="footer">
-            <a href=""> <img className="social-media" src={facebook} alt="facebook-img"/></a>
-            <a href="https://api.whatsapp.com/send?phone=+9629733949" ><img className="social-media" src={whatsapp} alt="whatsapp-img"/></a>
+         <h2 style={{color:'white',background:'#ef5350',width:'100%',padding:'5px 10px',fontSize:'calc(7px + 0.5vw)',textAlign:'center'}}>Report any error or wrong information to our Email</h2>
+            {/*<a href=""> <img className="social-media" src={facebook} alt="facebook-img"/></a>*/}
+            <a href="https://api.whatsapp.com/send?phone=+919629733949" ><img className="social-media" src={whatsapp} alt="whatsapp-img"/></a>
             <a href="https://instagram.com/spgvark_" target="_blank"><img className="social-media" src={instagram} alt="instagram-img"/></a>
             <a href="mailto:spgvarkpvtltd@gmail.com" target="_blank"><img className="social-media" src={gmail} alt="email-img"/></a>
             <p className="footer-txt" style={{color:' #fff'}}>© Copyright 2021 Spgvark</p>
